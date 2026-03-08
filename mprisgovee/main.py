80def48a2159d0e2ff391f0f13f695356f80b22f
obvRedwolf <ronaldpham32766@outlook.com>
2026-03-08 01:13:57 -0500

    add play/pause brightness logic

--- mprisgovee/main.py
+++ mprisgovee/main.py
@@ -2,7 +2,7 @@ import asyncio
 
 from .mpris import watch_metadata
 from .color import get_color
-from .govee import set_color
+from .govee import set_color, set_brightness
 from .cache import load_cache, save_cache
 from .config import load_config
 from .environment import check_environment
@@ -12,10 +12,26 @@ config = load_config()
 cache = load_cache()
 
 
-async def handle_art(url, player):
+async def handle_art(url, player, status):
     if player in config["ignored_apps"]:
         print(f"ignored {player}")
         return
+
+    print("\n")
+
+    if config["brightness_on_play_pause"]:
+        if (status == "Paused") or (status == "Stopped"):
+            set_brightness(
+                config["govee_ip"], config["govee_port"], config["brightness_paused"]
+            )
+            print(f"set brightness {config['brightness_playing']} from paused {player}")
+        elif status == "Playing":
+            set_brightness(
+                config["govee_ip"], config["govee_port"], config["brightness_playing"]
+            )
+            print(
+                f"set brightness {config['brightness_playing']} from playing {player}"
+            )
 
     if url in cache:
         print("cached color found")
