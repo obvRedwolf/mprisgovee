ed73ec757eeb4f93559bf4a5404eab279508cd8d
obvRedwolf <ronaldpham32766@outlook.com>
2026-03-07 21:37:24 -0500

    switch to toml, add app ignoring to config

--- mprisgovee/config.py
+++ mprisgovee/config.py
@@ -1,14 +1,32 @@
-import json
+import sys
+import tomllib
 
 from pathlib import Path
 
 CONFIG_DIR = Path.home() / ".config" / "mprisgovee"
-CONFIG_FILE = CONFIG_DIR / "config.json"
+CONFIG_FILE = CONFIG_DIR / "config.toml"
 
-DEFAULT_CONFIG = {
-    "govee_ip": "192.168.1.100",
-    "govee_port": 4003,
-}
+DEFAULT_CONFIG_TOML = """
+# ip address of your govee device
+# you can find this by going to your device's settings page, getting the mac, and matching it up with a device ip on your network
+govee_ip = "192.168.1.100"
+
+# lan port of your govee device
+# you can usually leave this default
+govee_port = 4003
+
+# apps to ignore ignore when looking for song art
+# by default, common browsers are ignored
+ignored_apps = [
+    "firefox",
+    "chromium",
+    "chrome",
+    "brave",
+    "vivaldi",
+    "zen",
+    "opera"
+]
+"""
 
 
 def load_config():
@@ -16,10 +34,13 @@ def load_config():
 
     if not CONFIG_FILE.exists():
         with open(CONFIG_FILE, "w") as f:
-            json.dump(DEFAULT_CONFIG, f, indent=4)
+            f.write(DEFAULT_CONFIG_TOML)
         print("running with first-launch default settings!")
         print(f'edit "{CONFIG_FILE}" to change settings!')
-        return DEFAULT_CONFIG
 
-    with open(CONFIG_FILE) as f:
-        return json.load(f)
+    try:
+        with open(CONFIG_FILE, "rb") as f:
+            return tomllib.load(f)
+    except Exception as e:
+        print(f"failed to load config: {e}")
+        sys.exit(1)
